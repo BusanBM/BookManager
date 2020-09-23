0d24d567962dc53dbfb169351f344394871442a2
sunkist365 <benjamin2894@nate.com>
2020-09-23 10:31:43 +0900

    삭제

--- book_project_an/WebContent/resources/js/book.js
+++
@@ -1,8 +0,0 @@
-function search_ok() {
-	if(document.search_bar.search_word.value.length==0){
-		alert("검색어를 입력해 주세요");
-		search_bar.search_word.focus();
-		return false;
-	}
-	document.search_bar.submit();
-}
